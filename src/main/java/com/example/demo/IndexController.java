baec38887519f4f1ab8d9bc2024ec049bfa63a9c
Stefan Bley <stefan.bley@saxsys.de>
2018-09-10 13:38:33 +0200

    Remove irrelevant code

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,13 +1,10 @@
 package com.example.demo;
 
 import org.springframework.hateoas.ResourceSupport;
-import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
