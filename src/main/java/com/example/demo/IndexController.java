8a0e99844e19d71e9f57ee626f0defc6d103516c
Stefan Bley <stefan.bley@saxsys.de>
2018-09-10 13:04:05 +0200

    Initial import

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public ResponseEntity<ResourceSupport> index() {
+
+        ResourceSupport index = new ResourceSupport();
+        index.add(linkTo(methodOn(IndexController.class).index()).withSelfRel());
+        return ResponseEntity.ok().body(index);
+    }
+}
---
+++ src/test/java/com/example/demo/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+public class IndexControllerTest {
+
+    @Autowired private TestRestTemplate clientRestTemplate;
+
+    @Test
+    public void responseShouldContainLinks() {
+        final ResponseEntity<ResourceSupport> response = clientRestTemplate.getForEntity("/", ResourceSupport.class);
+        assertThat(response.getBody().getLink("self")).isNotNull();
+    }
+}
